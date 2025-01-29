9a1a6ac4ba7e5ed97d9c1e80086888377e6d8621
devmedo48 <devmedo48@gmail.com>
2025-01-29 16:11:19 +0200

    Update socket.js

--- server/socket/socket.js
+++ server/socket/socket.js
@@ -7,7 +7,8 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173", "https://medo-social-app.vercel.app"],
+    // origin: ["http://localhost:5173", "https://medo-social-app.vercel.app"],
+    origin: "https://medo-social-app.vercel.app",
     methods: ["GET", "POST", "PUT"],
   },
 });
