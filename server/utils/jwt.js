b54d1594460450550dc6fe02d463eaab82c4202e
devmedo48 <devmedo48@gmail.com>
2025-01-29 14:32:19 +0200

    Update jwt.js

--- server/utils/jwt.js
+++ server/utils/jwt.js
@@ -11,10 +11,6 @@ export default async function jwtToken(userId, res) {
     "Access-Control-Allow-Methods",
     "GET,OPTIONS,PATCH,DELETE,POST,PUT"
   );
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
-  );
 
   res.cookie("token", token, {
     httpOnly: true,
