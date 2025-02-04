13d86278b115ab7f6418ae09c46e748c78322ae7
devmedo48 <devmedo48@gmail.com>
2025-02-04 13:12:49 +0200

    ll

--- server/index.js
+++ server/index.js
@@ -1,7 +1,6 @@
 import { configDotenv } from "dotenv";
 import express from "express";
 import cookieParser from "cookie-parser";
-import cors from "cors";
 import database from "./config/database.js";
 import mountRoutes from "./routes/index.js";
 import { v2 as cloudinary } from "cloudinary";
@@ -10,8 +9,6 @@ import { app, server } from "./socket/socket.js";
 configDotenv();
 database();
 
-let origin = ["http://localhost:5173", "https://medo-social-app.vercel.app"];
-app.use(cors({ origin }));
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
